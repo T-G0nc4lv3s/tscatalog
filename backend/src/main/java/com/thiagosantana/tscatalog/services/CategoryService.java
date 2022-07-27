65d39a9f3e1410277f552deb85b00f6511dedaf1
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-07-27 17:22:30 -0300

    Searching category by id

--- backend/src/main/java/com/thiagosantana/tscatalog/services/CategoryService.java
+++ backend/src/main/java/com/thiagosantana/tscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.thiagosantana.tscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +24,13 @@ public class CategoryService {
 		
 		return list.stream().map(cat -> new CategoryDTO(cat)).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+	}
+	
+	
 }
